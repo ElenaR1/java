8b8a18bf162fb896480817ebd70f62698e7d19be
ElenaR1 <elena.radoycheva@abv.bg>
2017-11-04 12:01:31 +0200

    Update MapShoppingCart.java

--- Upr4/MapShoppingCart.java
+++ Upr4/MapShoppingCart.java
@@ -1,5 +1,38 @@
 package bg.uni.sofia.fmi.mjt.shopping;
 
+import java.util.Collection;
+
 public class MapShoppingCart implements ShoppingCart {
+
+	private Map<Item,Integer> items;//quantity
+	@Override
+	public Collection<Item> getUniqueItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Item> getSortedItems() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addItem(Item item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeItem(Item item) throws ItemNotFoundException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double getTotal() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
