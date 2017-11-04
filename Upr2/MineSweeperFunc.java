86185215b6613ecc4a6b1d90b13d6ad585b4e804
ElenaR1 <elena.radoycheva@abv.bg>
2017-11-04 15:13:20 +0200

    Add files via upload

---
+++ Upr2/MineSweeperFunc.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+
+
+
+
+
+
+public class MineSweeperFunc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*Board obj=new Board();
+		int [][] arr=new int[3][3];
+		System.out.println(arr.length);
+		int a;
+		Scanner input=new Scanner(System.in);//we make a new scanner var
+        a=input.nextInt();
+        System.out.println(a);*/
+		Game obj=new Game();
+	}
+
+}
