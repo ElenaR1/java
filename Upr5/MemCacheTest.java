66555d793c7fcd13a5c6614a1c11f884ae459cb7
ElenaR1 <elena.radoycheva@abv.bg>
2017-11-11 11:59:06 +0200

    Add files via upload

---
+++ Upr5/MemCacheTest.java
@@ -0,0 +1,22 @@
+package bg.uni.sofia.fmi.mjt.cache;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+public class MemCacheTest {
+
+	@Test
+	public void test() {
+		LocalDateTime a=LocalDateTime.now();
+		Value obj=new Value(5,a);
+		Value obj2=new Value(6,a);
+		
+
+	}
+	
+
+}
