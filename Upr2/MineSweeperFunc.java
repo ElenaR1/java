2461b9f7da600c21292e2e6653cd86e0e839be5c
ElenaR1 <elena.radoycheva@abv.bg>
2018-10-17 11:37:04 +0300

    Update MineSweeperFunc.java

--- Upr2/MineSweeperFunc.java
+++ Upr2/MineSweeperFunc.java
@@ -1,6 +1,33 @@
 import java.util.Scanner;
 
 
+/******************************************************************************
+ *  Compilation:  javac Minesweeper.java
+ *  Execution:    java Minesweeper m n p
+ *  
+ *  Creates an MxN minesweeper game where each cell is a bomb with
+ *  probability p. Prints out the m-by-n game and the neighboring bomb
+ *  counts.
+ *
+ *  Sample execution:
+ *
+ *      % java Minesweeper  5 10 0.3
+ *      * . . . . . . . . * 
+ *      . . . . . . * . . . 
+ *      . . . . . . . . * * 
+ *      . . . * * * . . * . 
+ *      . . . * . . . . . . 
+ *
+ *      * 1 0 0 0 1 1 1 1 * 
+ *      1 1 0 0 0 1 * 2 3 3 
+ *      0 0 1 2 3 3 2 3 * * 
+ *      0 0 2 * * * 1 2 * 3 
+ *      0 0 2 * 4 2 1 1 1 1 
+ *
+ *A cell can contain a number or it can be blank. The number indicates how many mines are adjacent to thi
+ *s particular cell. We set a mark on a cell by right clicking on it. This way we indicate that we believe, 
+ *there is a mine. i po diagonal !
+ ******************************************************************************/
 
 
 
